eea29e948048538c1371d2b0dff1961a67dd3b90
unknown <welingtonnespolo@gmail.com>
2023-08-07 20:57:07 -0300

    atv-1

---
+++ atividade.py
@@ -0,0 +1,15 @@
+nome = "welington nespolo"
+idade = 2023 - 2005
+altura = 1,78
+peso = 68
+
+
+print (nome)
+print (str(idade)+ "anos")
+print (idade, "anos")
+print (str(altura) + "altura")
+print (str(peso) + "peso")
+if idade >= 18:
+    print("vc é maior de idade")
+else:
+    print("vc é menor de iadde" )
